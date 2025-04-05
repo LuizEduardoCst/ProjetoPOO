b9f344c204dd691502ee464ef17f43d16db09dc0
Luiz Eduardo Gomes <luizeduardo.le@ges.inatel.br>
2025-04-04 22:09:47 -0300

    Delete C:Users\luize\IdeaProjects\projetoPOO/SistemaOperacional.java

--- C:Users\luize\IdeaProjects\projetoPOO/SistemaOperacional.java
+++
@@ -1,9 +0,0 @@
-public class SistemaOperacional {
-    String nome;
-    int tipo; // 32 ou 64 bits
-
-    public SistemaOperacional(String nome, int tipo) {
-        this.nome = nome;
-        this.tipo = tipo;
-    }
-}
